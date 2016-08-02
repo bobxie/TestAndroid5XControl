9566f7d98b039b714bb78b780a3e0fdf845aea0c
bobxie <1583039459@qq.com>
2016-08-02 15:22:27 +0800

    init

--- app/src/main/java/cn/lovecoder/testandroid5xcontrol/BaseActivity.java
+++ app/src/main/java/cn/lovecoder/testandroid5xcontrol/BaseActivity.java
@@ -3,6 +3,7 @@ package cn.lovecoder.testandroid5xcontrol;
 import android.os.Bundle;
 import android.os.PersistableBundle;
 import android.support.v4.app.FragmentActivity;
+import android.util.Log;
 
 import cn.lovecoder.testandroid5xcontrol.util.StatusBarCompat;
 
@@ -12,10 +13,14 @@ import cn.lovecoder.testandroid5xcontrol.util.StatusBarCompat;
 public class BaseActivity extends FragmentActivity{
     @Override
     public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
-//        StatusBarCompat.compat(this);
         super.onCreate(savedInstanceState, persistentState);
-        StatusBarCompat.compat(this, getResources().getColor(R.color.colorAccent));
 //        StatusBarCompat.compat(this);
-//        StatusBarCompat.compat(this, getResources().getColor(R.color.colorAccent));
+    }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        Log.d("admin","hasFocus==" +hasFocus);
+        StatusBarCompat.compat(this, getResources().getColor(R.color.colorAccent));
     }
 }
--- app/src/main/java/cn/lovecoder/testandroid5xcontrol/MainActivity.java
+++ app/src/main/java/cn/lovecoder/testandroid5xcontrol/MainActivity.java
@@ -1,12 +1,17 @@
 package cn.lovecoder.testandroid5xcontrol;
 
+import android.app.Activity;
 import android.os.Bundle;
 
-public class MainActivity extends BaseActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+//       StatusBarCompat.compat(this, getResources().getColor(R.color.colorAccent));
+        //StatusBarCompat.compat(this);
     }
+
+
 }
